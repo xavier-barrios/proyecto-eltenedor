2f8c81c4b72f48154b95c6ea858680ea3c62336e
xavier barrios rodriguez <100005770.joan23@fje.edu>
2021-01-29 16:48:07 +0100

    asd

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -9,38 +9,22 @@ use Illuminate\Support\Facades\DB;
 class UsuariosController extends Controller
 {
     public function mostrar(){
-        //comprueba la sesion
-        // if (!session()->has('data')){
-        //     return redirect ('/');
-        // }
-        // coger todos los datos de la tabla restaurantes
-        // $lista=DB::table('restaurante')->get();
+    
+        // consulta para mostar los datos de los restaurantes en la vista mostrar
         $lista = DB::select('SELECT restaurante.nombre, restaurante.precio_medio, restaurante.foto, tipo.tipo_cocina, ubicacion.calle
         FROM restaurante 
             INNER JOIN tipo ON restaurante.id_tipo = tipo.id_tipo 
             INNER JOIN ubicacion ON restaurante.id_ubicacion = ubicacion.id_ubicacion');
+            // recorremos la variable lista para sacar las fotos y pasarlas a base 64 para mostrarlas por pantalla
             foreach ($lista as $i ) {
                 if ($i->foto !=null) {
                     $i->foto = base64_encode($i->foto);
                 }
             }
-        // return $lista;
-        // hace referencia a $lista y lo encia a mostrarvista
-        // compact -> pasarle mas de una variable a lista
         return view('mostrar', compact('lista'));
     }
 
-    // public function mostrar()
-    // {
-    //     $restaurante=DB::SELECT('SELECT restaurante.nombre, tipo.tipo_cocina, ubicacion.calle
-    //     FROM restaurante 
-    //         INNER JOIN tipo ON restaurante.id_tipo = tipo.id_tipo 
-    //         INNER JOIN ubicacion ON restaurante.id_ubicacion = ubicacion.id_ubicacion');
-    //         return view('mostrar', compact('lista'));
-    //     // return response()->json($restaurante, 200);
-    // }
-
-    // public function index(){
-    //     return view('mostrar');
-    // }
+    
+    
+    
 }
