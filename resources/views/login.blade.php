49f7d2cfa6b1691149c3f184e21db910a0922625
xavier barrios rodriguez <100005770.joan23@fje.edu>
2021-02-01 19:20:28 +0100

    favicon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" href="./img/favicon.ico" type="image/png" sizes="16x16">
     {{-- CSS --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/login.css')}}">
